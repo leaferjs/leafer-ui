c8eb2ddd40a1575a359dde079109b2d927e15b16
leaferjs <leaferjs@163.com>
2023-09-07 23:20:24 +0800

    update hoverData

--- packages/app/src/Leafer.ts
+++ packages/app/src/Leafer.ts
@@ -159,7 +159,10 @@ export class Leafer extends Group implements ILeafer {
     }
 
     public updateCursor(): void {
-        if (this.interaction) this.interaction.updateCursor()
+        if (this.interaction) {
+            this.interaction.updateHoverData()
+            this.interaction.updateCursor()
+        }
     }
 
     protected __doResize(size: IScreenSizeData): void {
