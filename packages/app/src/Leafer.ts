3ab4aee315a8e47cafec303232b4e351fa9b50f9
leaferjs <leaferjs@163.com>
2023-10-08 19:02:10 +0800

    count leafs

--- packages/app/src/Leafer.ts
+++ packages/app/src/Leafer.ts
@@ -71,6 +71,7 @@ export class Leafer extends Group implements ILeafer {
     public autoLayout?: IAutoBounds
 
     public get cursorPoint(): IPointData { return (this.interaction && this.interaction.hoverData) || { x: this.width / 2, y: this.height / 2 } }
+    public leafs = 0
 
     public __eventIds: IEventListenerId[] = []
     protected __startTimer: ITimer
