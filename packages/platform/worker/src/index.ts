40c148c7d2d1b26b2a648ee00aec57639392ff2f
leaferjs <leaferjs@163.com>
2023-12-27 11:33:17 +0800

    update interaction

--- packages/platform/worker/src/index.ts
+++ packages/platform/worker/src/index.ts
@@ -2,6 +2,12 @@ export * from '@leafer/worker'
 export * from '@leafer-ui/core'
 export * from '@leafer-ui/partner'
 
-import { useCanvas } from '@leafer/worker'
+import { useCanvas, Creator, InteractionBase, LeaferCanvas } from '@leafer/worker'
+import { ICreator } from '@leafer/interface'
+
+Object.assign(Creator, {
+    interaction: (target, canvas, selector, options?) => new InteractionBase(target, canvas, selector, options),
+    hitCanvas: (options?, manager?) => new LeaferCanvas(options, manager)
+} as ICreator)
 
 useCanvas('canvas')
