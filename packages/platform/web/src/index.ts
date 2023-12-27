0a71605bcc9fe6fccfad633040b39f4e60c2e958
leaferjs <leaferjs@163.com>
2023-12-27 11:26:37 +0800

    separation interaction

--- packages/platform/web/src/index.ts
+++ packages/platform/web/src/index.ts
@@ -1,7 +1,16 @@
 export * from '@leafer/web'
+export * from '@leafer/interaction-web'
+
 export * from '@leafer-ui/core'
 export * from '@leafer-ui/partner'
 
-import { useCanvas } from '@leafer/web'
+import { ICreator } from '@leafer/interface'
+import { useCanvas, Creator, LeaferCanvas } from '@leafer/web'
+import { Interaction } from '@leafer/interaction-web'
+
+Object.assign(Creator, {
+    interaction: (target, canvas, selector, options?) => new Interaction(target, canvas, selector, options),
+    hitCanvas: (options?, manager?) => new LeaferCanvas(options, manager)
+} as ICreator)
 
 useCanvas('canvas')
