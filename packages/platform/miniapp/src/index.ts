cd9e6f356ecae8764ac24a12251961cf5c52a61d
leaferjs <leaferjs@163.com>
2023-12-28 09:22:33 +0800

    update miniapp

--- packages/platform/miniapp/src/index.ts
+++ packages/platform/miniapp/src/index.ts
@@ -1,8 +1,17 @@
 export * from '@leafer/miniapp'
+export * from '@leafer/interaction-miniapp'
+
 export * from '@leafer-ui/core'
 export * from '@leafer-ui/partner'
 
-import { useCanvas } from '@leafer/miniapp'
+import { ICreator } from '@leafer/interface'
+import { useCanvas, Creator, LeaferCanvas } from '@leafer/miniapp'
+import { Interaction } from '@leafer/interaction-miniapp'
+
+Object.assign(Creator, {
+    interaction: (target, canvas, selector, options?) => new Interaction(target, canvas, selector, options),
+    hitCanvas: (options?, manager?) => new LeaferCanvas(options, manager)
+} as ICreator)
 
 try {
     useCanvas('wx', wx)
