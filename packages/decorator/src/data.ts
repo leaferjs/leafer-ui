10a2d4428bf547f07f2080dde5f05c7412bacb16
leaferjs <leaferjs@163.com>
2024-03-08 10:55:46 +0800

    update decoration

--- packages/decorator/src/data.ts
+++ packages/decorator/src/data.ts
@@ -1,57 +1,48 @@
 import { IStateStyleType, IValue, IObject } from '@leafer/interface'
-import { defineKey, defineLeafAttr, doStrokeType } from '@leafer/core'
+import { defineKey, decorateLeafAttr, attr, doStrokeType } from '@leafer/core'
 
 import { ICanvas, IUI } from '@leafer-ui/interface'
 import { State } from '@leafer-ui/external'
 
 
 export function stateType(defaultValue?: IValue) {
-    return (target: IUI, key: string) => {
-        const stateType = key + 'Style' as IStateStyleType
-        defineLeafAttr(target, key, defaultValue, {
-            set(value: IValue) {
-                this.__setAttr(key, value)
-                this.waitLeafer(() => { if (State.setStyle) State.setStyle(this, stateType, value as boolean) })
-            }
-        })
-    }
+    return decorateLeafAttr(defaultValue, (key: string) => attr({
+        set(value: IValue) {
+            this.__setAttr(key, value)
+            this.waitLeafer(() => { if (State.setStyle) State.setStyle(this, key + 'Style' as IStateStyleType, value as boolean) })
+        }
+    }))
 }
 
 export function arrowType(defaultValue?: IValue) {
-    return (target: IUI, key: string) => {
-        defineLeafAttr(target, key, defaultValue, {
-            set(value: IValue) {
-                this.__setAttr(key, value)
-                const data = (this as IUI).__
-                data.__useArrow = data.startArrow !== 'none' || data.endArrow !== 'none'
-                doStrokeType(this)
-            }
-        })
-    }
+    return decorateLeafAttr(defaultValue, (key: string) => attr({
+        set(value: IValue) {
+            this.__setAttr(key, value)
+            const data = (this as IUI).__
+            data.__useArrow = data.startArrow !== 'none' || data.endArrow !== 'none'
+            doStrokeType(this)
+        }
+    }))
 }
 
 export function effectType(defaultValue?: IValue) {
-    return (target: IUI, key: string) => {
-        defineLeafAttr(target, key, defaultValue, {
-            set(value: IValue) {
-                this.__setAttr(key, value)
-                if (value) (this as IUI).__.__useEffect = true
-                this.__layout.renderChanged || this.__layout.renderChange()
-            }
-        })
-    }
+    return decorateLeafAttr(defaultValue, (key: string) => attr({
+        set(value: IValue) {
+            this.__setAttr(key, value)
+            if (value) (this as IUI).__.__useEffect = true
+            this.__layout.renderChanged || this.__layout.renderChange()
+        }
+    }))
 }
 
 export function resizeType(defaultValue?: IValue) {
-    return (target: IUI, key: string) => {
-        defineLeafAttr(target, key, defaultValue, {
-            set(value: IValue) {
-                this.__setAttr(key, value)
-                this.__layout.boxChanged || this.__layout.boxChange();
-                (this as ICanvas).__updateSize()
-            }
-        })
-    }
+    return decorateLeafAttr(defaultValue, (key: string) => attr({
+        set(value: IValue) {
+            this.__setAttr(key, value)
+            this.__layout.boxChanged || this.__layout.boxChange();
+            (this as ICanvas).__updateSize()
+        }
+    }))
 }
 
 export function zoomLayerType() {
