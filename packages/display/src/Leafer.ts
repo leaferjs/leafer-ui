75efcd2dab148df2c0e6bd09545520fe4ed015f4
leaferjs <leaferjs@163.com>
2024-03-15 19:34:59 +0800

    update interaction window

--- packages/display/src/Leafer.ts
+++ packages/display/src/Leafer.ts
@@ -355,34 +355,9 @@ export class Leafer extends Group implements ILeafer {
     // need view plugin
     public zoom(_zoomType: IZoomType, _padding?: IFourNumber, _fixedScale?: boolean): IBoundsData { return undefined }
 
-    public validMove(moveX: number, moveY: number): IPointData {
-        const { scroll, disabled } = this.app.config.move
-        if (scroll) {
-            if (Math.abs(moveX) > Math.abs(moveY)) moveY = 0
-            else moveX = 0
-
-            if (scroll === 'limit') {
-                const { x, y, width, height } = new Bounds(this.__world).addPoint(this.zoomLayer as IPointData)
-                const right = x + width - this.width, bottom = y + height - this.height
-
-                if (x >= 0 && right <= 0) moveX = 0 // includeX
-                else if (moveX > 0) { if (x + moveX > 0) moveX = -x }
-                else if (moveX < 0 && right + moveX < 0) moveX = -right
-
-                if (y >= 0 && bottom <= 0) moveY = 0 // includeY
-                else if (moveY > 0) { if (y + moveY > 0) moveY = -y }
-                else if (moveY < 0 && bottom + moveY < 0) moveY = -bottom
-            }
-        }
-        return { x: disabled ? 0 : moveX, y: disabled ? 0 : moveY }
-    }
-
-    public validScale(changeScale: number): number {
-        const { scaleX } = this.zoomLayer.__, { min, max, disabled } = this.app.config.zoom, absScale = Math.abs(scaleX * changeScale)
-        if (absScale < min) changeScale = min / scaleX
-        else if (absScale > max) changeScale = max / scaleX
-        return disabled ? 1 : changeScale
-    }
+    // interaction window rewrite
+    public validMove(moveX: number, moveY: number): IPointData { return { x: moveX, y: moveY } }
+    public validScale(changeScale: number): number { return changeScale }
 
     protected __checkUpdateLayout(): void {
         this.__layout.update()
--- packages/type/src/index.ts
+++ packages/type/src/index.ts
@@ -1,6 +1,7 @@
 export { LeaferTypeCreator } from './LeaferTypeCreator'
 
-import { ILeaferType } from '@leafer/interface'
+import { ILeaferType, IPointData } from '@leafer/interface'
+import { Bounds } from '@leafer/core'
 
 import { Leafer } from '@leafer-ui/draw'
 
@@ -8,4 +9,33 @@ import { LeaferTypeCreator } from './LeaferTypeCreator'
 
 Leafer.prototype.initType = function (type: ILeaferType) {
     LeaferTypeCreator.run(type, this)
+}
+
+Leafer.prototype.validMove = function (moveX: number, moveY: number): IPointData {
+    const { scroll, disabled } = this.app.config.move
+    if (scroll) {
+        if (Math.abs(moveX) > Math.abs(moveY)) moveY = 0
+        else moveX = 0
+
+        if (scroll === 'limit') {
+            const { x, y, width, height } = new Bounds(this.__world).addPoint(this.zoomLayer as IPointData)
+            const right = x + width - this.width, bottom = y + height - this.height
+
+            if (x >= 0 && right <= 0) moveX = 0 // includeX
+            else if (moveX > 0) { if (x + moveX > 0) moveX = -x }
+            else if (moveX < 0 && right + moveX < 0) moveX = -right
+
+            if (y >= 0 && bottom <= 0) moveY = 0 // includeY
+            else if (moveY > 0) { if (y + moveY > 0) moveY = -y }
+            else if (moveY < 0 && bottom + moveY < 0) moveY = -bottom
+        }
+    }
+    return { x: disabled ? 0 : moveX, y: disabled ? 0 : moveY }
+}
+
+Leafer.prototype.validScale = function (changeScale: number): number {
+    const { scaleX } = this.zoomLayer.__, { min, max, disabled } = this.app.config.zoom, absScale = Math.abs(scaleX * changeScale)
+    if (absScale < min) changeScale = min / scaleX
+    else if (absScale > max) changeScale = max / scaleX
+    return disabled ? 1 : changeScale
 }
