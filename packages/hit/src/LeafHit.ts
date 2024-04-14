a583526e08ae262e69f08bfce1b6106775f6ab9b
leaferjs <leaferjs@163.com>
2024-04-14 08:22:54 +0800

    update hit code

--- packages/hit/src/LeafHit.ts
+++ packages/hit/src/LeafHit.ts
@@ -1,11 +1,13 @@
-import { IRadiusPointData } from '@leafer/interface'
+import { IRadiusPointData, ILeaferCanvas } from '@leafer/interface'
 import { Leaf, PointHelper, BoundsHelper } from '@leafer/core'
 
 
 const { toInnerRadiusPointOf, copy, setRadius } = PointHelper
 const inner = {} as IRadiusPointData
 
-Leaf.prototype.__hitWorld = function (point: IRadiusPointData): boolean {
+const leaf = Leaf.prototype
+
+leaf.__hitWorld = function (point: IRadiusPointData): boolean {
     if (this.__layout.hitCanvasChanged || !this.__hitCanvas) {
         this.__updateHitCanvas()
         if (!this.__layout.boundsChanged) this.__layout.hitCanvasChanged = false
@@ -24,3 +26,11 @@ Leaf.prototype.__hitWorld = function (point: IRadiusPointData): boolean {
 
     return this.__hit(inner)
 }
+
+leaf.__hitFill = function (inner: IRadiusPointData): boolean { return this.__hitCanvas?.hitFill(inner, this.__.windingRule) }
+
+leaf.__hitStroke = function (inner: IRadiusPointData, strokeWidth: number): boolean { return this.__hitCanvas?.hitStroke(inner, strokeWidth) }
+
+leaf.__hitPixel = function (inner: IRadiusPointData): boolean { return this.__hitCanvas?.hitPixel(inner, this.__layout.renderBounds, this.__hitCanvas.hitScale) }
+
+leaf.__drawHitPath = function (canvas: ILeaferCanvas): void { if (canvas) this.__drawRenderPath(canvas) }
