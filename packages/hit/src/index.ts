cc53a2effa3d8a8060511a2fde78dbe6d19e7ee9
leaferjs <leaferjs@163.com>
2024-04-17 10:57:34 +0800

    update pick

--- packages/hit/src/index.ts
+++ packages/hit/src/index.ts
@@ -7,15 +7,17 @@ import { IFindUIMethod, IUI } from '@leafer-ui/interface'
 import { UI, Group } from '@leafer-ui/draw'
 
 
-UI.prototype.find = function (condition: number | string | IFindUIMethod, options?: any): IUI[] {
+const ui = UI.prototype, group = Group.prototype
+
+ui.find = function (condition: number | string | IFindUIMethod, options?: any): IUI[] {
     return this.leafer ? this.leafer.selector.getBy(condition as IFindMethod, this, false, options) as IUI[] : []
 }
 
-UI.prototype.findOne = function (condition: number | string | IFindUIMethod, options?: any): IUI {
+ui.findOne = function (condition: number | string | IFindUIMethod, options?: any): IUI {
     return this.leafer ? this.leafer.selector.getBy(condition as IFindMethod, this, true, options) as IUI : null
 }
 
-Group.prototype.pick = function (hitPoint: IPointData, options?: IPickOptions): IPickResult {
+group.pick = function (hitPoint: IPointData, options?: IPickOptions): IPickResult {
     this.__layout.update()
     if (!options) options = {}
     return this.leafer ? this.leafer.selector.getByPoint(hitPoint, options.hitRadius || 0, { ...options, target: this }) : null
