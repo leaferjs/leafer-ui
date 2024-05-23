82f2f4f44529fc0a6827c7b28038b2bb727a42a0
leaferjs <leaferjs@163.com>
2024-05-23 12:09:17 +0800

    add scroll / offset / origin

--- packages/display/src/UI.ts
+++ packages/display/src/UI.ts
@@ -1,4 +1,4 @@
-import { ILeaferCanvas, IPathDrawer, IPathCommandData, IHitType, INumber, IBoolean, IString, IPathString, IExportFileType, IPointData, ICursorType, IMaskType, IAround, IValue, IWindingRule, IPathCreator, IFourNumber, IBoundsData, IFlowType, IGap, IWrap, IAxis, IConstraint, IAutoBoxData, IFlowBoxType, IPointGap, IFlowAlign, IFlowAxisAlign, IFindCondition, IAutoSize, IRangeSize } from '@leafer/interface'
+import { ILeaferCanvas, IPathDrawer, IPathCommandData, IHitType, INumber, IBoolean, IString, IPathString, IExportFileType, IPointData, ICursorType, IMaskType, IAround, IValue, IWindingRule, IPathCreator, IFourNumber, IBoundsData, IFlowType, IGap, IWrap, IAxis, IConstraint, IAutoBoxData, IFlowBoxType, IPointGap, IFlowAlign, IFlowAxisAlign, IFindCondition, IAutoSize, IRangeSize, IAlign, ILayoutData } from '@leafer/interface'
 import { Leaf, PathDrawer, surfaceType, dataType, positionType, boundsType, pathType, scaleType, rotationType, opacityType, sortType, maskType, dataProcessor, registerUI, useModule, rewrite, rewriteAble, UICreator, PathCorner, hitType, strokeType, PathConvert, eraserType, cursorType, autoLayoutType, pen, naturalBoundsType, pathInputType } from '@leafer/core'
 
 import { IUI, IShadowEffect, IBlurEffect, IStrokeAlign, IStrokeJoin, IStrokeCap, IBlendMode, IDashPatternString, IShadowString, IGrayscaleEffect, IUIData, IGroup, IStrokeWidthString, ICornerRadiusString, IUIInputData, IExportOptions, IExportResult, IFill, IStroke, IArrowType, IFindUIMethod, ILeafer, IEditorConfig, IEditorConfigFunction, IEditToolFunction } from '@leafer-ui/interface'
@@ -114,8 +114,25 @@ export class UI extends Leaf implements IUI { // tip: rewrited Box
     public skewY: INumber
 
 
+    // scroll
+    @positionType(0, true)
+    public scrollX: INumber
+
+    @positionType(0, true)
+    public scrollY: INumber
+
+    // offset
+    @positionType()
+    public offset: ILayoutData
+
+
+    // center
+    @positionType()
+    public origin: IAlign | IPointData
+
     @autoLayoutType()
     public around: IAround
+
 
     // image
     @dataType(false)
@@ -123,6 +140,7 @@ export class UI extends Leaf implements IUI { // tip: rewrited Box
 
     @naturalBoundsType(1)
     public pixelRatio: INumber
+
 
     // path
     @pathInputType()
@@ -133,6 +151,7 @@ export class UI extends Leaf implements IUI { // tip: rewrited Box
 
     @pathType(true)
     public closed: boolean
+
 
     // auto layout
     @autoLayoutType(false)
