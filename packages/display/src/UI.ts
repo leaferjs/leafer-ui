f83cd470e5efb49561c49cea3869410657ca0b84
leaferjs <leaferjs@163.com>
2024-06-05 09:37:54 +0800

    add lockRatio

--- packages/display/src/UI.ts
+++ packages/display/src/UI.ts
@@ -183,6 +183,9 @@ export class UI extends Leaf implements IUI { // tip: rewrited Box
     public autoHeight: IAutoSize
 
     @boundsType()
+    public lockRatio: IBoolean
+
+    @boundsType()
     public autoBox: IAutoBoxData | IConstraint
 
 
