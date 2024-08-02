4184ea9b273227a26bf4ee4741a6a7b74f4b779c
leaferjs <leaferjs@163.com>
2024-08-02 16:03:30 +0800

    update LeaferData

--- packages/display-module/data/src/LeaferData.ts
+++ packages/display-module/data/src/LeaferData.ts
@@ -1,8 +1,14 @@
-import { ILeaferData } from '@leafer-ui/interface'
+import { canvasSizeAttrs } from '@leafer/core'
+
+import { ILeaferData, IObject } from '@leafer-ui/interface'
 
 import { GroupData } from './GroupData'
 
-
 export class LeaferData extends GroupData implements ILeaferData {
 
+    public __getInputData(): IObject {
+        const data = super.__getInputData()
+        canvasSizeAttrs.forEach(key => delete (data as IObject)[key])
+        return data
+    }
 }
