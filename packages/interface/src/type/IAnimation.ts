49e8212d9d34fd2c2808367129d8baf6bbf491d7
leaferjs <leaferjs@163.com>
2024-08-21 17:37:50 +0800

    add easing type

--- packages/interface/src/type/IAnimation.ts
+++ packages/interface/src/type/IAnimation.ts
@@ -7,9 +7,24 @@ export type IKeyframe = IUIInputData | ITimelineKeyframe
 
 export type IKeyframeId = number
 
+export type IEasingType =
+    | 'linear'
+    | 'ease'
+    | 'ease-in' | 'ease-out' | 'ease-in-out'
+    | 'ease-in-sine' | 'ease-out-sine' | 'ease-in-out-sine'
+    | 'ease-in-quad' | 'ease-out-quad' | 'ease-in-out-quad'
+    | 'ease-in-cubic' | 'ease-out-cubic' | 'ease-in-out-cubic'
+    | 'ease-in-quart' | 'ease-out-quart' | 'ease-in-out-quart'
+    | 'ease-in-quint' | 'ease-out-quint' | 'ease-in-out-quint'
+    | 'ease-in-expo' | 'ease-out-expo' | 'ease-in-out-expo'
+    | 'ease-in-circ' | 'ease-out-circ' | 'ease-in-out-circ'
+    | 'ease-in-back' | 'ease-out-back' | 'ease-in-out-back'
+    | 'ease-in-elastic' | 'ease-out-elastic' | 'ease-in-out-elastic'
+    | 'ease-in-bounce' | 'ease-out-bounce' | 'ease-in-out-bounce'
+
 export interface ITimelineKeyframe {
     style: IUIInputData
-    easing?: string
+    easing?: string | number[] // number[] => cubic-bezier(number, number, number, number)
     delay?: number
     duration: number
     endDelay?: number
