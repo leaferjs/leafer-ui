14c1896f1078fbc45110b56469cce8a7f32d9a8c
leaferjs <leaferjs@163.com>
2024-09-04 08:26:35 +0800

    update state

--- packages/interface/src/module/IState.ts
+++ packages/interface/src/module/IState.ts
@@ -1,10 +1,10 @@
 import { ILeaf, IBoolean, IString } from '@leafer-ui/interface'
 
 export interface IStateModule {
-    isHover(leaf: ILeaf): boolean
-    isPress(leaf: ILeaf): boolean
-    isFocus(leaf: ILeaf): boolean
-    isDrag(leaf: ILeaf): boolean
+    isHover(leaf: ILeaf, checkButton?: boolean): boolean
+    isPress(leaf: ILeaf, checkButton?: boolean): boolean
+    isFocus(leaf: ILeaf, checkButton?: boolean): boolean
+    isDrag(leaf: ILeaf, checkButton?: boolean): boolean
 
     setStyle(leaf: ILeaf, styleName: IString, value: IBoolean): void
     setState(leaf: ILeaf, stateName: IString): void
