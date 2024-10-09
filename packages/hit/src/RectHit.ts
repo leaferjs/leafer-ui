6eac50dfd5eceb648de54710d321dda350042152
leaferjs <leaferjs@163.com>
2024-10-09 08:12:49 +0800

    fix Box hit

--- packages/hit/src/RectHit.ts
+++ packages/hit/src/RectHit.ts
@@ -1,18 +1,17 @@
 import { IRadiusPointData } from '@leafer/interface'
 import { BoundsHelper } from '@leafer/core'
-import { Rect, UI } from '@leafer-ui/draw'
+import { UI, Rect, Box } from '@leafer-ui/draw'
 
 
-const ui = new UI()
-const rect = Rect.prototype
+const ui = UI.prototype, rect = Rect.prototype, box = Box.prototype
 
 // hit 
 
-rect.__updateHitCanvas = function () {
+rect.__updateHitCanvas = box.__updateHitCanvas = function () {
     if (this.stroke || this.cornerRadius || ((this.fill || this.__.__isCanvas) && this.hitFill === 'pixel') || this.hitStroke === 'all') ui.__updateHitCanvas.call(this)
     else if (this.__hitCanvas) this.__hitCanvas = null
 }
 
-rect.__hitFill = function (inner: IRadiusPointData): boolean {
+rect.__hitFill = box.__hitFill = function (inner: IRadiusPointData): boolean {
     return this.__hitCanvas ? ui.__hitFill.call(this, inner) : BoundsHelper.hitRadiusPoint(this.__layout.boxBounds, inner)
 }
