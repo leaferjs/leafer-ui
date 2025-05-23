5047d124badd307383ab5df51a2bb96487688bd2
leaferjs <leaferjs@163.com>
2025-05-23 15:19:18 +0800

    fix(text): 修复文本使用letterSpacing后，emoji 显示不正常的问题

--- packages/partner/text/src/TextRows.ts
+++ packages/partner/text/src/TextRows.ts
@@ -36,6 +36,8 @@ export function createRows(drawData: ITextDrawData, content: string, style: ITex
         startCharSize = charWidth = charSize = wordWidth = rowWidth = 0
         word = { data: [] }, row = { words: [] }
 
+        if (__letterSpacing) content = [...content] as any // 防止切分表情等多个字符组成的字符，转成数组字符是最安全的
+
         for (let i = 0, len = content.length; i < len; i++) {
 
             char = content[i]
