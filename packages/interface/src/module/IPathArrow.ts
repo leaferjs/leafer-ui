2e65a692bfde2eb36e860245762ce0271954106e
leaferjs <leaferjs@163.com>
2025-08-26 14:54:51 +0800

    feat(PathArrow): 支持注册自定义箭头名称

--- packages/interface/src/module/IPathArrow.ts
+++ packages/interface/src/module/IPathArrow.ts
@@ -1,8 +1,10 @@
-import { IPathDataArrowMap } from '../type/IType'
+import { IPathDataArrow, IPathDataArrowMap } from '../type/IType'
 import { IUI } from '../IUI'
 
 
 export interface IPathArrowModule {
     list: IPathDataArrowMap
     addArrows(ui: IUI): void
+    register(name: string, data: IPathDataArrow): void
+    get(name: string): IPathDataArrow
 }
