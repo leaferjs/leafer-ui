4cec71c5674f0eaaac4541d95ac3c463b760875b
leaferjs <leaferjs@163.com>
2025-09-05 20:27:59 +0800

    feat(dragBounds): 支持限制resize操作

--- packages/display-module/bounds/src/DragBoundsHelper.ts
+++ packages/display-module/bounds/src/DragBoundsHelper.ts
@@ -1,39 +1,23 @@
-import { IPointData, IBoundsData, IDragBoundsType, ILeaf } from '@leafer/interface'
-import { MathHelper } from '@leafer/core'
+import { IPointData, IBoundsData, IDragBoundsType, ILeaf, ISide } from '@leafer/interface'
+import { Bounds, MathHelper } from '@leafer/core'
 
+
+const { float } = MathHelper
+const tempContent = new Bounds(), tempMerge = new Bounds(), tempIntersect = new Bounds()
 
 export const DragBoundsHelper = {
 
-    getValidMove(content: IBoundsData, dragBounds: IBoundsData, dragBoundsType: IDragBoundsType, move: IPointData, change?: boolean): IPointData {
-        const x = content.x + move.x, y = content.y + move.y, right = x + content.width, bottom = y + content.height
-        const boundsRight = dragBounds.x + dragBounds.width, boundsBottom = dragBounds.y + dragBounds.height
+    // 拖拽区域内移动
+    limitMove(leaf: ILeaf, move: IPointData): void {
+        const { dragBounds, dragBoundsType } = leaf
+        if (dragBounds) D.getValidMove(leaf.__localBoxBounds, D.getDragBounds(leaf), dragBoundsType, move, true)
+        D.axisMove(leaf, move)
+    },
 
-        if (!change) move = { ...move }
-
-        const isBiggerWidth = content.width > dragBounds.width
-        const isBiggerHeight = content.height > dragBounds.height
-
-        if (isBiggerWidth && dragBoundsType !== 'outer') {  // inner / auto 模式
-            if (x > dragBounds.x) move.x += dragBounds.x - x
-            else if (right < boundsRight) move.x += boundsRight - right
-        } else {
-            if (x < dragBounds.x) move.x += dragBounds.x - x
-            else if (right > boundsRight) move.x += boundsRight - right
-        }
-
-        if (isBiggerHeight && dragBoundsType !== 'outer') { // inner / auto 模式
-            if (y > dragBounds.y) move.y += dragBounds.y - y
-            else if (bottom < boundsBottom) move.y += boundsBottom - bottom
-        } else {
-            if (y < dragBounds.y) move.y += dragBounds.y - y
-            else if (bottom > boundsBottom) move.y += boundsBottom - bottom
-        }
-
-        // 避免出现很小为0的小数
-        move.x = MathHelper.float(move.x)
-        move.y = MathHelper.float(move.y)
-
-        return move
+    // 拖拽区域内缩放
+    limitScaleOf(leaf: ILeaf, origin: IPointData, scale: IPointData): void {
+        const { dragBounds, dragBoundsType } = leaf
+        if (dragBounds) D.getValidScaleOf(leaf.__localBoxBounds, D.getDragBounds(leaf), dragBoundsType, leaf.getLocalPointByInner(leaf.getInnerPointByBox(origin)), scale, true)
     },
 
     // 按轴移动
@@ -43,11 +27,69 @@ export const DragBoundsHelper = {
         if (draggable === 'y') move.x = 0
     },
 
-    // 拖拽区域内移动
-    limitMove(leaf: ILeaf, move: IPointData): void {
-        const { dragBounds, dragBoundsType } = leaf
-        if (dragBounds) D.getValidMove(leaf.__localBoxBounds, dragBounds === 'parent' ? leaf.parent.boxBounds : dragBounds, dragBoundsType, move, true)
-        D.axisMove(leaf, move)
+    getDragBounds(leaf: ILeaf): IBoundsData {
+        const { dragBounds } = leaf
+        return dragBounds === 'parent' ? leaf.parent.boxBounds : dragBounds
+    },
+
+    isInnerMode(content: IBoundsData, dragBounds: IBoundsData, dragBoundsType: IDragBoundsType, sideType: ISide): boolean {
+        return dragBoundsType === 'inner' || (dragBoundsType === 'auto' && content[sideType] > dragBounds[sideType])
+    },
+
+    getValidMove(content: IBoundsData, dragBounds: IBoundsData, dragBoundsType: IDragBoundsType, move: IPointData, change?: boolean): IPointData {
+        const x = content.x + move.x, y = content.y + move.y, right = x + content.width, bottom = y + content.height
+        const boundsRight = dragBounds.x + dragBounds.width, boundsBottom = dragBounds.y + dragBounds.height
+
+        if (!change) move = { ...move }
+
+        if (D.isInnerMode(content, dragBounds, dragBoundsType, 'width')) {  // inner 模式
+            if (x > dragBounds.x) move.x += dragBounds.x - x
+            else if (right < boundsRight) move.x += boundsRight - right
+        } else { // outer 模式
+            if (x < dragBounds.x) move.x += dragBounds.x - x
+            else if (right > boundsRight) move.x += boundsRight - right
+        }
+
+        if (D.isInnerMode(content, dragBounds, dragBoundsType, 'height')) { // inner 模式
+            if (y > dragBounds.y) move.y += dragBounds.y - y
+            else if (bottom < boundsBottom) move.y += boundsBottom - bottom
+        } else { // outer 模式
+            if (y < dragBounds.y) move.y += dragBounds.y - y
+            else if (bottom > boundsBottom) move.y += boundsBottom - bottom
+        }
+
+        // 避免出现很小为0的小数
+        move.x = float(move.x)
+        move.y = float(move.y)
+
+        return move
+    },
+
+    getValidScaleOf(content: IBoundsData, dragBounds: IBoundsData, dragBoundsType: IDragBoundsType, origin: IPointData, scale: IPointData, change?: boolean): IPointData {
+        if (!change) scale = { ...scale }
+
+        let fitScaleX: number, fitScaleY: number
+
+        tempContent.set(content).scaleOf(origin, scale.x, scale.y).unsign()
+        tempMerge.set(tempContent).add(dragBounds)
+        tempIntersect.set(tempContent).intersect(dragBounds)
+
+        if (D.isInnerMode(content, dragBounds, dragBoundsType, 'width')) {  // inner 模式
+            fitScaleX = tempMerge.width / tempContent.width
+        } else { // outer 模式
+            fitScaleX = tempIntersect.width / tempContent.width
+        }
+
+        if (D.isInnerMode(content, dragBounds, dragBoundsType, 'height')) { // inner 模式
+            fitScaleY = tempMerge.height / tempContent.height
+        } else { // outer 模式
+            fitScaleY = tempIntersect.height / tempContent.height
+        }
+
+        scale.x = float(tempIntersect.width) ? scale.x * fitScaleX : 1
+        scale.y = float(tempIntersect.height) ? scale.y * fitScaleY : 1
+
+        return scale
     }
 }
 
