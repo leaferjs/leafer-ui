13e75383f2bfd251b73e0dc78b0857fe1cc081df
leaferjs <leaferjs@163.com>
2025-09-08 09:15:47 +0800

    feat(textDecoration): 文本下划线支持offset

--- packages/partner/text/src/TextDecoration.ts
+++ packages/partner/text/src/TextDecoration.ts
@@ -5,21 +5,22 @@ import { ColorConvert } from '@leafer-ui/draw'
 
 
 export function decorationText(drawData: ITextDrawData, style: ITextData): void {
-    let type: ITextDecorationType
+    let type: ITextDecorationType, offset = 0
     const { fontSize, textDecoration } = style
     drawData.decorationHeight = fontSize / 11
     if (isObject(textDecoration)) {
         type = textDecoration.type
         if (textDecoration.color) drawData.decorationColor = ColorConvert.string(textDecoration.color)
+        if (textDecoration.offset) offset = Math.min(fontSize * 0.3, Math.max(textDecoration.offset, -fontSize * 0.15))
     } else type = textDecoration
     switch (type) {
         case 'under':
-            drawData.decorationY = [fontSize * 0.15]
+            drawData.decorationY = [fontSize * 0.15 + offset]
             break
         case 'delete':
             drawData.decorationY = [-fontSize * 0.35]
             break
         case 'under-delete':
-            drawData.decorationY = [fontSize * 0.15, -fontSize * 0.35]
+            drawData.decorationY = [fontSize * 0.15 + offset, -fontSize * 0.35]
     }
 }
