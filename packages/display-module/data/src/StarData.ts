8bff45493688183a2aec95dce4cad30edb5c6e01
leaferjs <leaferjs@163.com>
2026-06-26 07:33:23 +0800

    fix(Star): 星形采用外描边时会产生残影的问题

--- packages/display-module/data/src/StarData.ts
+++ packages/display-module/data/src/StarData.ts
@@ -5,6 +5,4 @@ import { UIData } from "./UIData"
 
 export class StarData extends UIData implements IStarData {
 
-    public get __boxStroke(): boolean { return !(this as IStarData).__pathInputed }
-
 }
